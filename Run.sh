413b16173290b166989b63ad4605c2fd44ef5c09
Ryan Williamson <ryan.williamson_99@hotmail.com>
2021-09-07 15:24:23 +1000

    Adding -p flag

--- Run.sh
+++ Run.sh
@@ -2,39 +2,39 @@
 echo "========================================"
 echo " Cleaning Up"
 make clean > /dev/null
-rm *.png
 echo " Compiling programs"
 make > /dev/null
+
 echo "========================================"
 echo " Running Serial Program"
 time ./mb5 10000 -0.668 0.32 0.02 > /dev/null
 echo "========================================"
 echo " Running GNUPLOT"
-gnuplot mandel.gp
+gnuplot -p mandel.gp
 mv mandel.png Images/mandelSerial.png
 echo "========================================"
 echo " Running Fork Program with 6 threads (Sockets)"
-time ./mbfs 10000 -0.668 0.32 0.02 6 > /dev/null
+time ./mbfs 10000 -0.668 0.32 0.002 6 > /dev/null
 echo "========================================"
 
 echo " Running GNUPLOT"
-gnuplot mandel.gp
+gnuplot -p mandel.gp
 mv mandel.png Images/mandelFork_Sockets.png
 
 echo "========================================"
 echo " Running Fork Program with 6 threads (Pipes)"
-time ./mbfp 10000 -0.668 0.32 0.02 6 > /dev/null
+time ./mbfp 10000 -0.668 0.32 0.002 6 > /dev/null
 echo "========================================"
 
 echo " Running GNUPLOT"
-gnuplot mandel.gp
+gnuplot -p mandel.gp
 mv mandel.png Images/mandelFork_Pipes.png
 
 echo "========================================"
 echo " Running PThreads Program with 6 threads"
-time ./mbp 10000 -0.668 0.32 0.02 6 > /dev/null
+time ./mbp 10000 -0.668 0.32 0.002 6 > /dev/null
 echo "========================================"
 
 echo " Running GNUPLOT"
-gnuplot mandel.gp
+gnuplot -p mandel.gp
 mv mandel.png Images/mandelFork_PThreads.png
